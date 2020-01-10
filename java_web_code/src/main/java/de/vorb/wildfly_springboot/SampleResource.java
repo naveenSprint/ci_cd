94efd68278f7a91cf94eb215a2c4ad67431932fd
naveenSprint <muppala.devops@gmail.com>
2020-01-10 12:01:15 +0530

    HJSAPP-2 #comment build this please #done

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -8,7 +8,7 @@ public class SampleResource {
 
     @RequestMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Hello World! This is to showcase a DevOps Automation End to End!";
     }
 
 }
